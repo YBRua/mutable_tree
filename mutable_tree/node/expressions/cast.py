8d4eccc099d4abcd00afebfcb5c10699c4027a69
YBRua <ybirua@sjtu.edu.cn>
2023-05-14 01:39:01 +0800

    added cast expr

---
+++ mutable_tree/node/expressions/cast.py
@@ -0,0 +1,24 @@
+from ..node import NodeType
+from .expression import Expression
+from ..miscs import TypeIdentifier
+from ..utils import is_expression
+
+
+class CastExpression(Expression):
+
+    def __init__(self, node_type: NodeType, type_identifier: TypeIdentifier,
+                 value: Expression):
+        super().__init__(node_type)
+        self.type = type_identifier
+        self.value = value
+
+    def _check_types(self):
+        if self.node_type != NodeType.CAST_EXPR:
+            raise TypeError(f'Invalid type: {self.node_type} for CastExpression.')
+        if not is_expression(self.value):
+            raise TypeError(f'Invalid type: {self.value.node_type} for Cast value.')
+        if self.type.node_type != NodeType.TYPE_IDENTIFIER:
+            raise TypeError(f'Invalid type: {self.type.node_type} for Cast type.')
+
+    def to_string(self) -> str:
+        return f'({self.type.to_string()}) {self.value.to_string()}'
