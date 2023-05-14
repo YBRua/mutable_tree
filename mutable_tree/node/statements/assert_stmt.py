1335765ac4aa144dd469bcab435b589ba5b2eb56
YBRua <ybirua@sjtu.edu.cn>
2023-05-14 12:12:07 +0800

    added assert stmt

---
+++ mutable_tree/node/statements/assert_stmt.py
@@ -0,0 +1,31 @@
+from ..node import NodeType
+from .statement import Statement
+from ..expressions import Expression
+from ..utils import is_expression
+from typing import Optional
+
+
+class AssertStatement(Statement):
+
+    def __init__(self,
+                 node_type: NodeType,
+                 condition: Expression,
+                 message: Optional[Expression] = None):
+        super().__init__(node_type)
+        self.condition = condition
+        self.message = message
+
+    def _check_types(self):
+        if self.node_type != NodeType.ASSERT_STMT:
+            raise TypeError(f'Invalid type: {self.node_type} for AssertStatement')
+        if not is_expression(self.condition):
+            raise TypeError(
+                f'Invalid type: {self.condition.node_type} for assert condition')
+        if self.message is not None and not is_expression(self.message):
+            raise TypeError(f'Invalid type: {self.message.node_type} for assert message')
+
+    def to_string(self) -> str:
+        if self.message is not None:
+            return f'assert {self.condition.to_string()} : {self.message.to_string()};\n'
+        else:
+            return f'assert {self.condition.to_string()};\n'
