cf6d47f43bf21bb03dd68227ce06847d59e85843
YBRua <ybirua@sjtu.edu.cn>
2023-05-15 11:28:36 +0800

    improved error handling in test framework

--- tests/test_java_expr.py
+++ tests/test_java_expr.py
@@ -14,7 +14,8 @@ class JavaExprTestBase(unittest.TestCase):
     def _stmt_round_trip(self, code: str):
         tree = self.parser.parse(code.encode())
         root = tree.root_node
-        self.assertFalse(root.has_error)
+        if root.has_error:
+            raise ValueError('original code is invalid')
 
         mutable_root = JavaAdaptor.convert_statement(root.children[0])
         new_code = mutable_root.to_string()
