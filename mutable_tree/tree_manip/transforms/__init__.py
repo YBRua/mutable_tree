b7e09027e3be50620e2831b20ef346d2e2e1e300
YBRua <ybirua@sjtu.edu.cn>
2023-05-20 19:39:18 +0800

    updated incr/decr transform

--- mutable_tree/tree_manip/transforms/__init__.py
+++ mutable_tree/tree_manip/transforms/__init__.py
@@ -5,3 +5,5 @@ from .loop_for import WhileToForVisitor
 # increment/decrement transformers
 from .update_prefix import PrefixUpdateVisitor
 from .update_postfix import PostfixUpdateVisitor
+from .update_assign import AssignUpdateVisitor
+from .update_binop import BinopUpdateVisitor
--- tests/test_increment_transform.py
+++ tests/test_increment_transform.py
@@ -1,13 +1,14 @@
 import unittest
 from .transform_test_base import TransformTestBase
-from mutable_tree.tree_manip.transforms import (PrefixUpdateVisitor, PostfixUpdateVisitor)
+from mutable_tree.tree_manip.transforms import (PrefixUpdateVisitor, PostfixUpdateVisitor,
+                                                AssignUpdateVisitor, BinopUpdateVisitor)
 
 
 class TestIncrementTransform(TransformTestBase):
 
     def test_prefix_update(self):
         visitor = PrefixUpdateVisitor()
-        verbose = True
+        verbose = False
         self.check_transform('++i;', visitor, verbose=verbose)
         self.check_transform('i++;', visitor, verbose=verbose)
         self.check_transform('--i;', visitor, verbose=verbose)
@@ -33,7 +34,59 @@ class TestIncrementTransform(TransformTestBase):
 
     def test_postfix_update(self):
         visitor = PostfixUpdateVisitor()
-        verbose = True
+        verbose = False
+        self.check_transform('++i;', visitor, verbose=verbose)
+        self.check_transform('i++;', visitor, verbose=verbose)
+        self.check_transform('--i;', visitor, verbose=verbose)
+        self.check_transform('i--;', visitor, verbose=verbose)
+
+        self.check_transform('i = i + 1;', visitor, verbose=verbose)
+        self.check_transform('i = i - 1;', visitor, verbose=verbose)
+
+        self.check_transform('i += 1;', visitor, verbose=verbose)
+        self.check_transform('i -= 1;', visitor, verbose=verbose)
+
+        self.check_transform('i = j + 1;', visitor, verbose=verbose)
+        self.check_transform('i = j - 1;', visitor, verbose=verbose)
+        self.check_transform('i = 1 + i;', visitor, verbose=verbose)
+        self.check_transform('i = 1 - i;', visitor, verbose=verbose)
+        self.check_transform('i = i + 2;', visitor, verbose=verbose)
+        self.check_transform('i = i - 2;', visitor, verbose=verbose)
+
+        self.check_transform('for (int i = 0; i < 10; i++) { print(i); }',
+                             visitor,
+                             verbose=verbose)
+        self.check_transform('while (i < 10) { i++; }', visitor, verbose=verbose)
+
+    def test_assign_update(self):
+        visitor = AssignUpdateVisitor()
+        verbose = False
+        self.check_transform('++i;', visitor, verbose=verbose)
+        self.check_transform('i++;', visitor, verbose=verbose)
+        self.check_transform('--i;', visitor, verbose=verbose)
+        self.check_transform('i--;', visitor, verbose=verbose)
+
+        self.check_transform('i = i + 1;', visitor, verbose=verbose)
+        self.check_transform('i = i - 1;', visitor, verbose=verbose)
+
+        self.check_transform('i += 1;', visitor, verbose=verbose)
+        self.check_transform('i -= 1;', visitor, verbose=verbose)
+
+        self.check_transform('i = j + 1;', visitor, verbose=verbose)
+        self.check_transform('i = j - 1;', visitor, verbose=verbose)
+        self.check_transform('i = 1 + i;', visitor, verbose=verbose)
+        self.check_transform('i = 1 - i;', visitor, verbose=verbose)
+        self.check_transform('i = i + 2;', visitor, verbose=verbose)
+        self.check_transform('i = i - 2;', visitor, verbose=verbose)
+
+        self.check_transform('for (int i = 0; i < 10; i++) { print(i); }',
+                             visitor,
+                             verbose=verbose)
+        self.check_transform('while (i < 10) { i++; }', visitor, verbose=verbose)
+
+    def test_binop_update(self):
+        visitor = BinopUpdateVisitor()
+        verbose = False
         self.check_transform('++i;', visitor, verbose=verbose)
         self.check_transform('i++;', visitor, verbose=verbose)
         self.check_transform('--i;', visitor, verbose=verbose)
