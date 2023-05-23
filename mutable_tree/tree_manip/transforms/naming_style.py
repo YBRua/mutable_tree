1f668de0de48f4c27bd5e3b47193073d17b50397
flyboss <915741005@qq.com>
2023-05-23 15:53:03 +0800

    Update naming_style.py

--- mutable_tree/tree_manip/transforms/naming_style.py
+++ mutable_tree/tree_manip/transforms/naming_style.py
@@ -5,41 +5,28 @@ from typing import Optional
 import inflection
 
 
-# def is_all_lowercase(name) -> bool:
-#     return name.lower() == name
-#
-#
-# def is_all_uppercase(name) -> bool:
-#     return name.upper() == name
-#
-#
-# def is_camel_case(name) -> bool:
-#     if is_all_lowercase(name):
-#         return False
-#     if not name[0].isalpha():
-#         return False
-#     return inflection.camelize(name, uppercase_first_letter=False) == name
-#
-#
-# def is_pascal_case(name) -> bool:
-#     if is_all_uppercase(name):
-#         return False
-#     if not name[0].isalpha():
-#         return False
-#     return inflection.camelize(name, uppercase_first_letter=True) == name
-#
-#
-# def is_snake_case(name) -> bool:
-#     return '_' in name.strip('_')
-
-
 def is_underscore_case(name) -> bool:
     return name[0] == '_' and name[1:].strip('_') != ''
 
 
-class ToCamelCaseVisitor(TransformingVisitor):
+class VarNamingVisitor(TransformingVisitor):
     def __init__(self):
-        self.__variable_name_mapping = {}
+        self.variable_name_mapping = {}
+
+    def visit_Identifier(self,
+                         node: Identifier,
+                         parent: Optional[Node] = None,
+                         parent_attr: Optional[str] = None):
+        name = node.name
+        new_name = self.variable_name_mapping.get(name, None)
+        if new_name is not None:
+            decl_id = node_factory.create_identifier(new_name)
+            return True, [decl_id]
+        else:
+            return False, []
+
+
+class ToCamelCaseVisitor(VarNamingVisitor):
 
     def visit_VariableDeclarator(self,
                                  node: VariableDeclarator,
@@ -50,28 +37,14 @@ class ToCamelCaseVisitor(TransformingVisitor):
             new_name = inflection.camelize(name[1:], uppercase_first_letter=False)
         else:
             new_name = inflection.camelize(name, uppercase_first_letter=False)
-        self.__variable_name_mapping[name] = new_name
+        self.variable_name_mapping[name] = new_name
 
         decl_id = node_factory.create_identifier(new_name)
         variable_declarator = node_factory.create_variable_declarator(decl_id)
         return True, [variable_declarator]
 
-    def visit_Identifier(self,
-                         node: Identifier,
-                         parent: Optional[Node] = None,
-                         parent_attr: Optional[str] = None):
-        name = node.name
-        new_name = self.__variable_name_mapping.get(name, None)
-        if new_name is not None:
-            decl_id = node_factory.create_identifier(new_name)
-            return True, [decl_id]
-        else:
-            return False, []
 
-
-class ToPascalCaseVisitor(TransformingVisitor):
-    def __init__(self):
-        self.__variable_name_mapping = {}
+class ToPascalCaseVisitor(VarNamingVisitor):
 
     def visit_VariableDeclarator(self,
                                  node: VariableDeclarator,
@@ -82,28 +55,14 @@ class ToPascalCaseVisitor(TransformingVisitor):
             new_name = inflection.camelize(name[1:], uppercase_first_letter=True)
         else:
             new_name = inflection.camelize(name, uppercase_first_letter=True)
-        self.__variable_name_mapping[name] = new_name
+        self.variable_name_mapping[name] = new_name
 
         decl_id = node_factory.create_identifier(new_name)
         variable_declarator = node_factory.create_variable_declarator(decl_id)
         return True, [variable_declarator]
 
-    def visit_Identifier(self,
-                         node: Identifier,
-                         parent: Optional[Node] = None,
-                         parent_attr: Optional[str] = None):
-        name = node.name
-        new_name = self.__variable_name_mapping.get(name, None)
-        if new_name is not None:
-            decl_id = node_factory.create_identifier(new_name)
-            return True, [decl_id]
-        else:
-            return False, []
 
-
-class ToSnakeCaseVisitor(TransformingVisitor):
-    def __init__(self):
-        self.__variable_name_mapping = {}
+class ToSnakeCaseVisitor(VarNamingVisitor):
 
     def visit_VariableDeclarator(self,
                                  node: VariableDeclarator,
@@ -114,28 +73,14 @@ class ToSnakeCaseVisitor(TransformingVisitor):
             new_name = inflection.underscore(name[1:])
         else:
             new_name = inflection.underscore(name)
-        self.__variable_name_mapping[name] = new_name
+        self.variable_name_mapping[name] = new_name
 
         decl_id = node_factory.create_identifier(new_name)
         variable_declarator = node_factory.create_variable_declarator(decl_id)
         return True, [variable_declarator]
 
-    def visit_Identifier(self,
-                         node: Identifier,
-                         parent: Optional[Node] = None,
-                         parent_attr: Optional[str] = None):
-        name = node.name
-        new_name = self.__variable_name_mapping.get(name, None)
-        if new_name is not None:
-            decl_id = node_factory.create_identifier(new_name)
-            return True, [decl_id]
-        else:
-            return False, []
 
-
-class ToUnderscoreCaseVisitor(TransformingVisitor):
-    def __init__(self):
-        self.__variable_name_mapping = {}
+class ToUnderscoreCaseVisitor(VarNamingVisitor):
 
     def visit_VariableDeclarator(self,
                                  node: VariableDeclarator,
@@ -145,7 +90,7 @@ class ToUnderscoreCaseVisitor(TransformingVisitor):
         if not is_underscore_case(name):
             name = '_' + name
             new_name = inflection.underscore(name)
-            self.__variable_name_mapping[name] = new_name
+            self.variable_name_mapping[name] = new_name
 
             decl_id = node_factory.create_identifier(new_name)
             variable_declarator = node_factory.create_variable_declarator(decl_id)
@@ -160,7 +105,7 @@ class ToUnderscoreCaseVisitor(TransformingVisitor):
         name = node.name
         if not is_underscore_case(name):
             name = '_' + name
-            new_name = self.__variable_name_mapping.get(name, None)
+            new_name = self.variable_name_mapping.get(name, None)
             if new_name is not None:
                 decl_id = node_factory.create_identifier(new_name)
                 return True, [decl_id]
