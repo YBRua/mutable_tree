7e9aff17397ced14e80f0911790c9a1496cfbf18
YBRua <ybirua@sjtu.edu.cn>
2024-01-02 11:58:02 +0800

    CI/CD, fixed tree-sitter lib version

--- ci/scripts/build_tree_sitter.sh
+++ ci/scripts/build_tree_sitter.sh
@@ -2,10 +2,21 @@
 mkdir tree-sitter
 cd tree-sitter
 
-# currently only Java is supported
+# currently only certain versions of tree-sitter are supported
 git clone https://github.com/tree-sitter/tree-sitter-java.git
+cd tree-sitter-java
+git checkout 6c8329e2da78fae78e87c3c6f5788a2b005a4afc
+cd ..
+
 git clone https://github.com/tree-sitter/tree-sitter-cpp.git
+cd tree-sitter-cpp
+git checkout 0e7b7a02b6074859b51c1973eb6a8275b3315b1d
+cd ..
+
 git clone https://github.com/tree-sitter/tree-sitter-javascript.git
+cd tree-sitter-javascript
+git checkout f772967f7b7bc7c28f845be2420a38472b16a8ee
+cd ..
 
 # return to base directory
 cd ..
